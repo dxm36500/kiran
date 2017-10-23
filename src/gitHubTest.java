ddea8a864928e939b121b2429c851e817c716392
Kiran <sxk25890@ucmo.edu>
2017-10-22 22:01:24 -0500

    six
    akjsdhas

--- src/gitHubTest.java
+++ src/gitHubTest.java
@@ -4,7 +4,7 @@ public class gitHubTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Demo git hub project ");
+		System.out.println("Demo git hub  ");
 
 	}
 
